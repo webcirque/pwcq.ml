fa1f1b392950def8ec4fc0a3ffc29cba98c80826
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-02-08 12:45:03 +0800

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -1,3 +1,6 @@
 if (window.navigator.userAgent.search("Mobile") != -1) {
-  document.body.style.width = "500px";
+  document.body.style = "max-width:480px;";
+  mob = document.createElement("p");
+  mob.innerHTML = "You are using mobile vision.";
+  document.body.appendChild(mob);
 }
