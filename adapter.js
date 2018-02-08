2d70b35839154f7aded911c769b127c44e7da84e
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-02-08 12:55:56 +0800

    Update adapter.js

--- adapter.js
+++ adapter.js
@@ -1,10 +1,12 @@
 try {
-if (window.navigator.userAgent.search("Mobile") != -1) {
-  document.getElementsByTagName("body")[0].style = "max-width:480px;";
-  mob = document.createElement("p");
-  mob.innerHTML = "You are using mobile vision.";
-  document.body.appendChild(mob);
-}
+  window.onload = function () {
+    if (window.navigator.userAgent.search("Mobile") != -1) {
+      document.getElementsByTagName("body")[0].style = "max-width:480px;";
+      mob = document.createElement("p");
+      mob.innerHTML = "You are using mobile vision.";
+      document.body.appendChild(mob);
+    }
+  }
 }
 catch (err) {
   alert(err.stack);
