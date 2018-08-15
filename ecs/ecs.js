ced2e338045955790957ae15a079505b7d0ae8ab
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-15 15:38:00 +0800

    Update ecs.js

--- ecs/ecs.js
+++ ecs/ecs.js
@@ -5,6 +5,29 @@ window.onload = function () {
 	content = document.getElementById("ecs-content");
 	content.style.width = (window.outerWidth - 30) + "px";
 	encr = location.hash.replace("#","");
+	function _styler_() {
+			txt = content.innerHTML.split("\n");
+		donum = 0;
+		while (donum < txt.length) {
+			if (txt[donum].split(":")[0] == "t1") {
+				txt[donum] = "<h1>" + txt[donum].replace("t1:","") + "</h1>";
+			} else if (txt[donum].split(":")[0] == "t2") {
+				txt[donum] = "<h2>" + txt[donum].replace("t2:","") + "</h2>";
+			} else if (txt[donum].split(":")[0] == "t3") {
+				txt[donum] = "<h3>" + txt[donum].replace("t3:","") + "</h3>";
+			} else {
+				txt[donum] = "<p>" + txt[donum].replace("p:","") + "</p>";
+			}
+			donum ++;
+		}
+		donum = 0;
+		text = "";
+		while (donum < txt.length) {
+			text += txt[donum] + "\n";
+			donum ++;
+		}
+		content.innerHTML = text;
+	}
 	if (encr != "" ) {
 		//If URL has something to parse, it will parse.
 		console.log("[ECS] Get text to parse.");
@@ -34,26 +57,8 @@ window.onload = function () {
 				content.innerHTML += "t1:<span style=\"color: red\">Corrupted link!</span>";
 			}
 		};
+		_styler_();
+	} else {
+		_styler_();
 	}
-	txt = content.innerHTML.split("\n");
-	donum = 0;
-	while (donum < txt.length) {
-		if (txt[donum].split(":")[0] == "t1") {
-			txt[donum] = "<h1>" + txt[donum].replace("t1:","") + "</h1>";
-		} else if (txt[donum].split(":")[0] == "t2") {
-			txt[donum] = "<h2>" + txt[donum].replace("t2:","") + "</h2>";
-		} else if (txt[donum].split(":")[0] == "t3") {
-			txt[donum] = "<h3>" + txt[donum].replace("t3:","") + "</h3>";
-		} else {
-			txt[donum] = "<p>" + txt[donum].replace("p:","") + "</p>";
-		}
-		donum ++;
-	}
-	donum = 0;
-	text = "";
-	while (donum < txt.length) {
-		text += txt[donum] + "\n";
-		donum ++;
-	}
-	content.innerHTML = text;
 }
