d67994a7ef306c65c1aeb3885835870e26cfa133
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-15 13:32:39 +0800

    Update ecs.js

--- ecs/ecs.js
+++ ecs/ecs.js
@@ -1,4 +1,29 @@
 window.onload = function () {
 	content = document.getElementById("ecs-content");
 	content.style.width = (window.outerWidth - 30) + "px";
+	encr = location.hash.replace("#","");
+	if (encr != "" ) {
+		//If URL has something to parse, it will parse.
+	}
+	let txt = content.innerHTML.split("\n");
+	let donum = 0;
+	while (donum < txt.length) {
+		if (txt[donum].split(":")[0] == "t1") {
+			txt[donum] = "<h1>" + txt[donum] + "</h1>";
+		} else if (txt[donum].split(":")[0] == "t2") {
+			txt[donum] = "<h2>" + txt[donum] + "</h2>";
+		} else if (txt[donum].split(":")[0] == "t3") {
+			txt[donum] = "<h3>" + txt[donum] + "</h3>";
+		} else {
+			txt[donum] = "<p>" + txt[donum] + "</p>";
+		}
+		donum ++;
+	}
+	donum = 0;
+	let text = "";
+	while (donum < txt.length) {
+		text += txt[donum] + "\n";
+		donum ++;
+	}
+	content.innerHTML = text;
 }
