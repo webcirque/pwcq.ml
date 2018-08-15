9e38b25552ddb6cdeba135795161dfb58d36f828
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-15 13:37:12 +0800

    Update ecs.js

--- ecs/ecs.js
+++ ecs/ecs.js
@@ -9,13 +9,13 @@ window.onload = function () {
 	let donum = 0;
 	while (donum < txt.length) {
 		if (txt[donum].split(":")[0] == "t1") {
-			txt[donum] = "<h1>" + txt[donum] + "</h1>";
+			txt[donum] = "<h1>" + txt[donum].replace("t1:","") + "</h1>";
 		} else if (txt[donum].split(":")[0] == "t2") {
-			txt[donum] = "<h2>" + txt[donum] + "</h2>";
+			txt[donum] = "<h2>" + txt[donum].replace("t2:","") + "</h2>";
 		} else if (txt[donum].split(":")[0] == "t3") {
-			txt[donum] = "<h3>" + txt[donum] + "</h3>";
+			txt[donum] = "<h3>" + txt[donum].replace("t3:","") + "</h3>";
 		} else {
-			txt[donum] = "<p>" + txt[donum] + "</p>";
+			txt[donum] = "<p>" + txt[donum].replace("p:","") + "</p>";
 		}
 		donum ++;
 	}
