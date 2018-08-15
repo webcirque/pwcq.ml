3e18658a23336d2d4702bff28e6d880cfa53494e
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-08-15 15:56:23 +0800

    Update ecs.js

--- ecs/ecs.js
+++ ecs/ecs.js
@@ -23,41 +23,44 @@ function styler() {
 	}
 encr = location.hash.replace("#","");
 if (encr != "" ) {
-		//If URL has something to parse, it will parse.
-		console.log("[ECS] Get text to parse.");
-		content.innerHTML = "t1:Parsing your link...";
-		_import(":base64").main = function () {
-			console.log("[ECS] Base64");
-			b64 = new Base64();
-			b64.mode(2);
-			elist = b64.decode(encr);
-			elist = elist.split(":");
-			b64.mode(0);
-			console.log("[ECS] Encryption method: " + elist[1] + ".");
-			console.log("[ECS] Encryption key: " + elist[0] + ".");
-			ctt = elist[2];
-			console.log("[ECS] Encrypted text: " + ctt + ".");
-			enclist = ["base64","aes","des","rabbit"];
-			if (elist[1].toLowerCase() == "base64") {
-				console.log("[ECS]No need to decrypt");
-				context = b64.decode(ctt);
-				content.innerHTML = context;
-			} else if (elist[1].toLowerCase()) {
-				console.log("[ECS]Need to decrypt by " + elist[1].toUpperCase() + ".");
-				_import(":crypto").main = function () {
-					console.log("[ECS] Crypto");
-				}
-			} else {
-				console.error("[ECS]Unknown encryption!");
-				content.innerHTML += "t1:<span style=\"color: red\">Corrupted link!</span>";
-			}
-		};
-	} 
+	//If URL has something to parse, it will parse.
+	console.log("[ECS] Get text to parse.");
+	content.innerHTML = "t1:Parsing your link...";
+	_import(":base64").main = function () {
+		console.log("[ECS] Base64");
+		b64 = new Base64();
+		b64.mode(2);
+		elist = b64.decode(encr);
+		elist = elist.split(":");
+		b64.mode(0);
+		console.log("[ECS] Encryption method: " + elist[1] + ".");
+		console.log("[ECS] Encryption key: " + elist[0] + ".");
+		ctt = elist[2];
+		console.log("[ECS] Encrypted text: " + ctt + ".");
+		enclist = ["base64","aes","des","rabbit"];
+	}
+} else {
+	elist = ["0","LLL",""];
+}
 window.onload = function () {
 	document.body.style.width = (window.outerWidth) + "px";
 	header = document.getElementById("ecs-header");
 	header.style.width = (window.outerWidth) + "px";
 	content = document.getElementById("ecs-content");
 	content.style.width = (window.outerWidth - 30) + "px";
+	if (elist[1].toLowerCase() == "base64") {
+		console.log("[ECS] No need to decrypt");
+		context = b64.decode(ctt);
+		console.log("[ECS] Decrypted!");
+		content.innerHTML = context;
+	} else if (enclist.indexOf(elist[1].toLowerCase()) != -1) {
+		console.log("[ECS]Need to decrypt by " + elist[1].toUpperCase() + ".");
+		_import(":crypto").main = function () {
+			console.log("[ECS] Crypto");
+		}
+	} else {
+		console.error("[ECS]Unknown encryption!");
+		content.innerHTML += "t1:<span style=\"color: red\">Corrupted link!</span>";
+	}
 	styler();
 }
