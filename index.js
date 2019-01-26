c0f4d0ac41daa15ceaf05fdd009984ac56e759fe
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2019-01-26 20:22:05 +0800

    Bug fixes

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ if ('serviceWorker' in navigator) {
 document.onreadystatechange = function () {
 	if (this.readyState.toLowerCase() == "interactive") {
 		// Try to load some elements
-		contentCards = Array.from(document.getElementsByClassName("content-card"))
+		contentCards = Array.from(document.getElementsByClassName("content-card"));
+		navTabs = Array.from(document.querySelectorAll("ul.nav-tabs li"));
 		resizeElements();
-	}
-}
+	} else if (this.readyState.toLowerCase() == "complete") {
+		resizeElements();
+	};
+};
 
 // If window resizes
 window.onresize = function () {
@@ -19,14 +22,18 @@ window.onresize = function () {
 
 // Resize elements
 function resizeElements () {
+	// Resizing cards
 	let heightArray = [];
 	contentCards.forEach((e, i) => {
-		if (innerWidth < 840) {
+		e.style.height = "";
+		if (innerWidth <= 360) {
 			e.style.width = (innerWidth - 52).toString() + "px";
+		} else if (innerWidth <= 840) {
+			e.style.width = (innerWidth - 34).toString() + "px";
 		} else {
-			e.style.width = (innerWidth / 2 - 42).toString() + "px";
+			e.style.width = (innerWidth / 2 - 34).toString() + "px";
 		};
-		heightArray.push(e.clientHeight);
+		heightArray.push(parseInt(getComputedStyle(e).height));
 	});
 	contentCards.forEach((e, i) => {
 		if (innerWidth >= 840) {
@@ -35,4 +42,18 @@ function resizeElements () {
 			e.style.height = currentHeight.toString() + "px";
 		}
 	});
+	// Resizing tabs
+	navTabs.forEach((e) => {
+		if (innerWidth <= 840) {
+			e.style.width = (innerWidth - 34).toString() + "px";
+		} else {
+			e.style.width = "";
+		}
+	});
+	// Change background behaviour
+	if ((innerWidth / innerHeight) < 0.75) {
+		document.body.style.backgroundSize = "auto";
+	} else {
+		document.body.style.backgroundSize = "";
+	}
 }
